e1b4ee0c03e0ca18a7d014293f4d87dad124fa06
ruaanls <ruanlima257@hotmail.com>
2025-07-27 13:52:36 -0300

    Commit inicial

---
+++ src/main/java/br/com/fiap/minibanco/MiniBancoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.minibanco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniBancoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniBancoApplication.class, args);
+    }
+
+}
